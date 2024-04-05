bb5f536ae9ea16e002e68341c15c8d24bcee503f
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-05 03:42:59 +0200

    refactor code

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,3 +1,9 @@
-with open('./books/frankenstein.txt') as f:
-    for line in f:
-        print(line)
+def main():
+    print(open_book())
+
+def open_book():
+    with open('./books/frankenstein.txt') as f:
+        content = f.read()
+        return content
+
+main()
