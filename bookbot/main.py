bec073d3bb7b5cceb9db899a4f22092cff7a7fd0
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-06 19:40:48 +0200

    count words of the document

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,9 +1,16 @@
 def main():
-    print(open_book())
+    book_content = get_content()
+    words = count_words(book_content)
+    print(words)
 
-def open_book():
+def get_content():
     with open('./books/frankenstein.txt') as f:
         content = f.read()
         return content
+
+def count_words(book):
+    words = book.split()
+    words_count = len(words)
+    return words_count
 
 main()
