27bc20bcaeabaf501d91efbb043d2e4e6bc939da
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-08 23:10:44 +0200

    get urls from html

--- web_crawler/crawl.js
+++ web_crawler/crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require("jsdom");
+
 function normalizeURL(url) {
   if (!url) {
     return "no url provided";
@@ -13,6 +15,29 @@ function normalizeURL(url) {
   return fullPath;
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody);
+  aElements = dom.window.document.querySelectorAll("a");
+  const urls = [];
+  for (const aElement of aElements) {
+    if (aElement.href.slice(0, 1) === "/") {
+      try {
+        urls.push(new URL(aElement.href, baseURL).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
+    }
+  }
+  return urls;
+}
+
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
