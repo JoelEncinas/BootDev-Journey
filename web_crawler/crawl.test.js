0e29e66769f96f8714e317524be6c2600d1fe368
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-08 21:38:38 +0200

    add bootdev tests

--- web_crawler/crawl.test.js
+++ web_crawler/crawl.test.js
@@ -1,4 +1,5 @@
 const { normalizeURL } = require("./crawl.js");
+const { test, expect } = require("@jest/globals");
 
 test("pass url and normalize it", () => {
   expect(normalizeURL("https://www.wikipedia.org/")).toBe("www.wikipedia.org");
@@ -6,4 +7,33 @@ test("pass url and normalize it", () => {
 
 test("log err if no url is provided", () => {
   expect(normalizeURL()).toBe("no url provided");
+});
+
+// boot dev tests
+test("normalizeURL protocol", () => {
+  const input = "https://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL capitals", () => {
+  const input = "https://BLOG.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL http", () => {
+  const input = "http://BLOG.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
 });
