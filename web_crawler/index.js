336b730efd7b498b39b0f3f51d0f20aa8fbdea8d
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-08 23:27:45 +0200

    exit script if no url or too many urls

--- web_crawler/index.js
+++ web_crawler/index.js
@@ -4,11 +4,18 @@ function main(baseURL) {
 
 const args = process.argv.slice(2); // First two arguments are node and the script name
 
-if(args.length < 1){
-    console.log("invalid syntax!")
-    console.log("npm run start YOUR_URL")
+if (args.length < 1) {
+  console.log("invalid syntax!");
+  console.log("npm run start YOUR_URL");
+  process.exit(1);
 }
 
-const baseURL = args[0]; // The first argument after 'npm run start'
+if (args.length > 1) {
+  console.log("invalid syntax! the script only accepts 1 url at a time");
+  console.log("npm run start YOUR_URL");
+  process.exit(1);
+}
+
+const baseURL = args[0]; 
 
 main(baseURL);
