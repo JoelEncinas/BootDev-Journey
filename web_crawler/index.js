ab02062049db79f37ac7893633317bceef0c6218
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-08 23:25:56 +0200

    check if user has inputed any url

--- web_crawler/index.js
+++ web_crawler/index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+function main(baseURL) {
+  //
+}
+
+const args = process.argv.slice(2); // First two arguments are node and the script name
+
+if(args.length < 1){
+    console.log("invalid syntax!")
+    console.log("npm run start YOUR_URL")
+}
+
+const baseURL = args[0]; // The first argument after 'npm run start'
+
+main(baseURL);
