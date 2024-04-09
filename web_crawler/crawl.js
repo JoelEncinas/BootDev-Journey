e27791647d27a1fb17ad4250d5aee3cc4d834ba1
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-09 19:10:22 +0200

    first version of the crawler, it only crawls the host related links

--- web_crawler/crawl.js
+++ web_crawler/crawl.js
@@ -15,6 +15,21 @@ function normalizeURL(url) {
   return fullPath;
 }
 
+function getHostFromUrl(url) {
+  if (!url) {
+    return "no url provided";
+  }
+
+  const urlToNormalize = new URL(url);
+  fullPath = `${urlToNormalize.host}`;
+
+  if (fullPath.length > 0 && fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
+  }
+
+  return fullPath;
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const dom = new JSDOM(htmlBody);
   aElements = dom.window.document.querySelectorAll("a");
@@ -34,44 +49,78 @@ function getURLsFromHTML(htmlBody, baseURL) {
       }
     }
   }
-  console.log(urls);
-
   return urls;
 }
 
-function crawlPage(baseURL, currentURL, pages) {
-  fetch(baseURL)
-    .then((response) => {
-      if (!response.ok) {
-        if (response.status >= 400 && response.status < 500) {
-          throw new Error("Client error: " + response.status);
-        } else if (response.status >= 500 && response.status < 600) {
-          throw new Error("Server error: " + response.status);
-        } else {
-          throw new Error("Unknown error: " + response.status);
-        }
-      }
+function crawlPage(baseURL, currentURL, pages = {}) {
+  return new Promise((resolve, reject) => {
+    if (pages[currentURL]) {
+      pages[currentURL] = pages[currentURL] + 1;
+      resolve(pages);
+    } else {
+      pages[currentURL] = 1;
 
-      const contentType = response.headers.get("content-type");
-      if (!contentType || !contentType.includes("text/html")) {
-        throw new Error("Content needs to be html");
-      }
+      fetch(currentURL)
+        .then((response) => {
+          if (!response.ok) {
+            if (response.status >= 400 && response.status < 500) {
+              throw new Error("Client error: " + response.status);
+            } else if (response.status >= 500 && response.status < 600) {
+              throw new Error("Server error: " + response.status);
+            } else {
+              throw new Error("Unknown error: " + response.status);
+            }
+          }
 
-      return response.text();
-    })
-    .then((html) => {
-      // add all the urls to the pages object
-      getURLsFromHTML(html, baseURL);
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
+          const contentType = response.headers.get("content-type");
+          if (!contentType || !contentType.includes("text/html")) {
+          } else {
+            return response.text();
+          }
+        })
+        .then((html) => {
+          links = getURLsFromHTML(html, baseURL);
 
-  // call the function recursively here
+          const promises = [];
+
+          for (const link of links) {
+            if (link.startsWith("/")) {
+              const absoluteUrl = new URL(link, baseURL).href;
+              if (absoluteUrl.startsWith(baseURL)) {
+                promises.push(crawlPage(baseURL, absoluteUrl, pages));
+              }
+            } else if (link.startsWith(baseURL)) {
+              promises.push(crawlPage(baseURL, link, pages));
+            }
+          }
+
+          Promise.all(promises).then(() => resolve(pages));
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
+    }
+  });
+}
+
+function validateArgs(args) {
+  if (args.length < 1) {
+    console.log("invalid syntax!");
+    console.log("npm run start YOUR_URL");
+    process.exit(1);
+  }
+
+  if (args.length > 1) {
+    console.log("invalid syntax! the script only accepts 1 url at a time");
+    console.log("npm run start YOUR_URL");
+    process.exit(1);
+  }
 }
 
 module.exports = {
   normalizeURL,
   getURLsFromHTML,
   crawlPage,
+  validateArgs,
+  getHostFromUrl,
 };
--- web_crawler/index.js
+++ web_crawler/index.js
@@ -1,26 +1,20 @@
-const { crawlPage } = require("./crawl.js");
+const { crawlPage, validateArgs } = require("./crawl.js");
 
 function main() {
   const args = process.argv.slice(2); // First two arguments are node and the script name
 
-  if (args.length < 1) {
-    console.log("invalid syntax!");
-    console.log("npm run start YOUR_URL");
-    process.exit(1);
-  }
-
-  if (args.length > 1) {
-    console.log("invalid syntax! the script only accepts 1 url at a time");
-    console.log("npm run start YOUR_URL");
-    process.exit(1);
-  }
+  validateArgs(args);
 
   const baseURL = args[0];
   console.log("Initializing the crawler...");
 
-  let pages = {};
-
-  crawlPage(baseURL, baseURL, pages);
+  crawlPage(baseURL, baseURL)
+    .then((pages) => {
+      console.log(pages);
+    })
+    .catch((error) => {
+      console.error("Crawling failed:", error);
+    });
 }
 
 main();
