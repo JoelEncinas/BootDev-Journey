b9eb3fa48e45c2f2fde3c723480ce9c5c0493395
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-09 19:50:36 +0200

    printed the data sorted

--- web_crawler/src/report.js
+++ web_crawler/src/report.js
@@ -1,6 +1,14 @@
 function printReport(pages) {
+  const pagesArray = Object.entries(pages);
+  pagesArray.sort((a, b) => b[1] - a[1]);
+  const sortedPages = {};
+
+  pagesArray.forEach(([key, value]) => {
+    sortedPages[key] = value;
+  });
+
   console.log("\n");
-  Object.entries(pages).forEach(([key, value]) => {
+  Object.entries(sortedPages).forEach(([key, value]) => {
     console.log(`${key} : ${value}`);
   });
 }
